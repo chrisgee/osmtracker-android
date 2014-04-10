e266d21b9365775a48c55368f71323b6ebd2ee4e
Christoph Gohle <ctg@mpq.mpg.de>
2014-04-10 05:24:30 +0200

    start editable waypoints (work in progress)

---
+++ src/me/guillaumin/android/osmtracker/activity/WaypointDetail.java
@@ -0,0 +1,7 @@
+package me.guillaumin.android.osmtracker.activity;
+
+import android.Activity;
+
+public class WaypointDetail extends Activity {
+
+}
--- src/me/guillaumin/android/osmtracker/activity/WaypointList.java
+++ src/me/guillaumin/android/osmtracker/activity/WaypointList.java
@@ -1,11 +1,25 @@
 package me.guillaumin.android.osmtracker.activity;
 
+import me.guillaumin.android.osmtracker.OSMTracker;
+import me.guillaumin.android.osmtracker.R;
 import me.guillaumin.android.osmtracker.db.TrackContentProvider;
 import me.guillaumin.android.osmtracker.db.TrackContentProvider.Schema;
 import me.guillaumin.android.osmtracker.db.WaypointListAdapter;
+import me.guillaumin.android.osmtracker.gpx.ExportToStorageTask;
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
+import android.preference.PreferenceManager;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 
 /**
  * Activity that lists the previous waypoints tracked by the user.
@@ -41,4 +55,114 @@ public class WaypointList extends ListActivity {
 		super.onPause();
 	}
 
+	/**
+	 * User has clicked a waypoint.
+	 * @param lv listview; this
+	 * @param iv item clicked
+	 * @param position position within list
+	 * @param id  waypoint ID
+	 */
+	@Override
+	protected void onListItemClick(ListView lv, View iv, final int position, final long id) {
+		Intent i;
+		i = new Intent(this, WaypointDetail.class);
+		i.putExtra(Schema.COL_TRACK_ID, id);
+		}
+		startActivity(i);
+	}
+
+
+	
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		getMenuInflater().inflate(R.menu.trackmgr_contextmenu, menu);
+		
+		long selectedId = ((AdapterContextMenuInfo) menuInfo).id;
+		menu.setHeaderTitle(getResources().getString(R.string.trackmgr_contextmenu_title).replace("{0}", Long.toString(selectedId)));
+		if(currentTrackId == selectedId){
+			// the selected one is the active track, so we will show the stop item
+			menu.findItem(R.id.trackmgr_contextmenu_stop).setVisible(true);
+		}else{
+			// the selected item is not active, so we need to hide the stop item
+			menu.findItem(R.id.trackmgr_contextmenu_stop).setVisible(false);
+		}
+		menu.setHeaderTitle(getResources().getString(R.string.trackmgr_contextmenu_title).replace("{0}", Long.toString(selectedId)));
+		if ( currentTrackId ==  selectedId) {
+			// User has pressed the active track, hide the delete option
+			menu.removeItem(R.id.trackmgr_contextmenu_delete);
+		}
+	}
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		final AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		
+		Intent i;
+		
+		switch(item.getItemId()) {
+		case R.id.:
+			// stop the active track
+			stopActiveTrack();
+			break;
+		case R.id.trackmgr_contextmenu_resume:
+			// let's activate the track and start the TrackLogger activity
+			setActiveTrack(info.id);
+			i = new Intent(this, TrackLogger.class);
+			i.putExtra(Schema.COL_TRACK_ID, info.id);
+			startActivity(i);
+			break;
+		case R.id.trackmgr_contextmenu_delete:
+			
+			// Confirm and delete selected track
+			new AlertDialog.Builder(this)
+				.setTitle(R.string.trackmgr_contextmenu_delete)
+				.setMessage(getResources().getString(R.string.trackmgr_delete_confirm).replace("{0}", Long.toString(info.id)))
+				.setCancelable(true)
+				.setIcon(android.R.drawable.ic_dialog_alert)
+				.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						deleteTrack(info.id);
+						dialog.dismiss();
+					}
+				})
+				.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.cancel();
+					}
+				}).create().show();
+	
+			break;
+		case R.id.trackmgr_contextmenu_export:	
+			new ExportToStorageTask(this, info.id).execute();
+			break;
+		case R.id.trackmgr_contextmenu_osm_upload:
+			i = new Intent(this, OpenStreetMapUpload.class);
+			i.putExtra(Schema.COL_TRACK_ID, info.id);
+			startActivity(i);
+			break;
+		case R.id.trackmgr_contextmenu_display:
+			// Start display track activity, with or without OSM background
+			boolean useOpenStreetMapBackground = PreferenceManager.getDefaultSharedPreferences(this).getBoolean(
+					OSMTracker.Preferences.KEY_UI_DISPLAYTRACK_OSM, OSMTracker.Preferences.VAL_UI_DISPLAYTRACK_OSM);
+			if (useOpenStreetMapBackground) {
+				i = new Intent(this, DisplayTrackMap.class);
+			} else {
+				i = new Intent(this, DisplayTrack.class);
+			}
+			i.putExtra(Schema.COL_TRACK_ID, info.id);
+			startActivity(i);
+			break;
+		case R.id.trackmgr_contextmenu_details:
+			i = new Intent(this, TrackDetail.class);
+			i.putExtra(Schema.COL_TRACK_ID, info.id);
+			startActivity(i);
+			break;
+		}
+		return super.onContextItemSelected(item);
+	}
+	
 }
